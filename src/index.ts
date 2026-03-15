6de8a9c3912555eb7f12fc3be6617b36b9e2a9bd
IndifferentGhost <12449668+indifferentghost@users.noreply.github.com>
2026-03-14 20:25:53 -0700

    Add `@base` Layout Inline
    Added a base html:5 layout inline, adding the `@` let's the eta engine know to look for the layout in-memory.

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,23 @@ import { Eta } from 'eta';
 const app = new Hono();
 const eta = new Eta();
 
+const baseLayout = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title><%= it.title %></title>
+</head>
+<body>
+  <%~ it.body %>
+</body>
+</html>`;
+
+eta.loadTemplate('@base', baseLayout);
+
 app.get('/', (c) => {
-  return c.text(eta.renderString('Hello <%= it.name %>', { name: 'Node.JS' }));
+  return c.html(eta.renderString('<% layout("@base") %><h1>Hello <%= it.name %></h1>', { name: 'Node.JS', title: 'home' }));
 })
 
 serve({

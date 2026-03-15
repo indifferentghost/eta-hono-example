f10d180ce2a8445c1f93afba9d5bea9f9d5f4619
IndifferentGhost <12449668+indifferentghost@users.noreply.github.com>
2026-03-14 21:01:31 -0700

    Moved base layout and defined views config
    This moves the _base_ layout, previously defined inline and in-memory as `@base` to it's own folder in `src/views/layout/base.eta` and defined the `views` configuration option to point to the views folder.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,15 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
 import { Eta } from 'eta';
+import { join } from 'node:path';
 
 const app = new Hono();
-const eta = new Eta();
-
-const baseLayout = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><%= it.title %></title>
-</head>
-<body>
-  <%~ it.body %>
-</body>
-</html>`;
-
-eta.loadTemplate('@base', baseLayout);
+const eta = new Eta({
+  views: join(import.meta.dirname, './views/'),
+});
 
 app.get('/', (c) => {
-  return c.html(eta.renderString('<% layout("@base") %><h1>Hello <%= it.name %></h1>', { name: 'Node.JS', title: 'home' }));
+  return c.html(eta.renderString('<% layout("layouts/base") %><h1>Hello <%= it.name %></h1>', { name: 'Node.JS', title: 'home' }));
 })
 
 serve({

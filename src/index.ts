ac169a4c42fd7d4dcede92481ab101fcf09e7802
IndifferentGhost <12449668+indifferentghost@users.noreply.github.com>
2026-03-14 20:03:00 -0700

    Install Import and Render Eta
    This is the basics for implementing Eta in Hono. It instanciates an Eta instance and executes `renderString` applying the defined variables ot the inline template.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { Eta } from 'eta';
 
-const app = new Hono()
+const app = new Hono();
+const eta = new Eta();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text(eta.renderString('Hello <%= it.name %>', { name: 'Node.JS' }));
 })
 
 serve({
   fetch: app.fetch,
-  port: 3000
+  port: 3000,
 }, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+  console.log(`Server is running on http://localhost:${info.port}`);
+});
